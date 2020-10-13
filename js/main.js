db6c13dc0d1f29c173f6009ec12b5fcee4a354e1
Ana Martinez Fernandez <anamartinezf6@gmail.com>
2020-10-13 09:38:52 +0200

    Add updateClick Function - js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const inputNumber = document.querySelector (".js-input-number");
 const submitBtn = document.querySelector (".js-submit-button");
 const clueEl = document.querySelector (".js-clue");
 const attemptsEl = document.querySelector (".js-attempts");
-
+let attemptsNumber = 0;
 
 //HANDLER
 //1. Generador de número aleatorio
@@ -13,4 +13,16 @@ const attemptsEl = document.querySelector (".js-attempts");
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
   }
- 
+
+
+//2. Generador de pistas 
+function updateClick () {
+  console.log("entro en la función updateClick");
+  const random = getRandomNumber(100);
+  console.log(`Mi número aleatorio es: ${random}`);
+  let inputNumberValue = inputNumber.value;
+  console.log(`El número introducido en el formulario es: ${inputNumberValue}`); 
+  
+}  
+//Listener
+submitBtn.addEventListener ("click", updateClick);
